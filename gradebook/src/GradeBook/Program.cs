9b0c925e85c7feef7a61bbf496dbc2066aa0ce1c
Thomas <thomas.meche@gmail.com>
2020-08-07 13:04:51 -0600

    Testing the Main method and passing command line args

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,9 +6,17 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            string userName = Console.ReadLine();
-            Console.WriteLine("Hello " + userName);
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("What is your name?");
+                string userName = Console.ReadLine();
+                Console.WriteLine($"Hello {userName}");
+            }
         }
     }
 }
