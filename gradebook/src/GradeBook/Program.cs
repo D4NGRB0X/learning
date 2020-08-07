4a2d71950b784c764066ffdba991f1914327ba8d
Thomas <thomas.meche@gmail.com>
2020-08-07 12:31:20 -0600

    First commit of C# Gradebook Console app

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName);
+        }
+    }
+}
