1175b7e1d3a897e8fb551c080e6e4f6c0dd399dc
Thomas <thomas.meche@gmail.com>
2020-08-14 13:24:51 -0600

    Started exploring collections in C# build and manipulated arrays

---
+++ C# Collections/DaysOfWeek/DaysOfWeek/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DaysOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thorrsday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            };
+
+
+            /*while (true)
+            {
+                Console.WriteLine("Which day do you want to display?");
+                Console.WriteLine("(Mon = 1, etc.) >");
+                int dayIndex = int.Parse(Console.ReadLine());
+
+                string chosenDay = daysOfWeek[dayIndex - 1];
+                Console.WriteLine($"That day is {chosenDay}");
+
+                if(chosenDay == "q")
+                {
+                    break;
+                }
+            }*/
+            foreach(string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+            daysOfWeek[3] = "Thursday";
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
