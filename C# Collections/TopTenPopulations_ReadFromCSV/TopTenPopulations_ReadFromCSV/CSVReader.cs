6b0fd800fb8a3bab50efa531cedffc2eb5f8ce52
Thomas <thomas.meche@gmail.com>
2020-08-17 10:36:50 -0600

    Begin working with data from external source

---
+++ C# Collections/TopTenPopulations_ReadFromCSV/TopTenPopulations_ReadFromCSV/CSVReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TopTenPopulations_ReadFromCSV
+{
+    class CSVReader
+    {
+        private string _csvFilePath; //field
+
+        public CSVReader(string csvFilePath) //constructor using field
+        {
+            this._csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries) //method returning Array of country objects that is length n
+        {
+            Country[] countries = new Country[nCountries];
+            return countries;
+        }
+    }
+}
---
+++ C# Collections/TopTenPopulations_ReadFromCSV/TopTenPopulations_ReadFromCSV/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TopTenPopulations_ReadFromCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C: \Users\Thomas\Desktop\learning\Course_Files\C# Collections\Pop by Largest Final.csv";
+            CSVReader reader = new CSVReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(9);
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{country.Population}: {country.Name}");
+            }
+        }
+    }
+}
