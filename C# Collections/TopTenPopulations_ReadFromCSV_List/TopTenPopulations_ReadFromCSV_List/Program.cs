bf352da64a8d841d4a1084cd203bca09932ad8f1
Thomas <thomas.meche@gmail.com>
2020-08-19 09:53:39 -0600

    Updated CSVReader class to handle known issue with data (Some data will be broken into array of 5 elements rather than 4) checked for length; implemented switch statment to handle edge cases

---
+++ C# Collections/TopTenPopulations_ReadFromCSV_List/TopTenPopulations_ReadFromCSV_List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace TopTenPopulations_ReadFromCSV_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Thomas\Desktop\learning\Course_Files\C# Collections\Pop by Largest Final.csv";
+            CSVReader reader = new CSVReader(filePath);
+
+            List<Country> countries = reader.ReadAllCountries();
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{country.Population}: {country.Name}");
+            }
+        }
+    }
+}
