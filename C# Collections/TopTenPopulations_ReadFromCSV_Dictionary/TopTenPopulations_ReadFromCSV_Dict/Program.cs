5f2d6b565071072d3bb0bd921f326c976d40c955
Thomas <thomas.meche@gmail.com>
2020-08-20 10:09:25 -0600

    Working with Dictionary<TKey, TValue>

---
+++ C# Collections/TopTenPopulations_ReadFromCSV_Dictionary/TopTenPopulations_ReadFromCSV_Dict/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace TopTenPopulations_ReadFromCSV_Dict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Thomas\Desktop\learning\Course_Files\C# Collections\Pop by Largest Final.csv";
+            CSVReader reader = new CSVReader(filePath);
+
+            Dictionary<string, Country> countries = reader.ReadAllCountries();
+            Console.WriteLine("Please enter a country code: ");
+            string userInput = Console.ReadLine();
+            
+            bool countryExsists = countries.TryGetValue(userInput.ToUpper(), out Country country);
+            if(!countryExsists)
+                Console.WriteLine($"Sorry, the country code you entered is invalid, {userInput}");
+            else
+                Console.WriteLine($"{country.Name} has a population of {country.Population}");
+        }
+    }
+}
