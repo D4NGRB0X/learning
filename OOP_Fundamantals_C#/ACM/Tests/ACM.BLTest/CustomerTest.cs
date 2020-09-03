a64e83842252e8a631f5552fd56319f78ccd22cb
Thomas <thomas.meche@gmail.com>
2020-09-03 10:18:03 -0600

    continued writing test for customer class

--- OOP_Fundamantals_C#/ACM/Tests/ACM.BLTest/CustomerTest.cs
+++ OOP_Fundamantals_C#/ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -23,7 +23,37 @@ namespace ACM.BLTest
             //--ASSERT
             Assert.Equal(expected, actual);
         }
-    
 
+        [Fact]
+        public void FullNameFirstNameEmpty()
+        {    //--ARRANGE
+            Customer customer = new Customer
+            { 
+                LastName = "Baggins" 
+            };
+            string expected = "Baggins";
+            //--ACT
+            string actual = customer.FullName;
+            //--ASSERT
+            Assert.Equal(expected, actual);
+        }
+
+       [Fact]
+       public void FullNameLastNameEmpty()
+        {
+            //--ARRANGE
+            Customer customer = new Customer
+            { 
+                FirstName = "Bilbo" 
+            };
+
+            string expected = "Bilbo";
+
+            //--ACT
+            string actual = customer.FullName;
+
+            //--ASSERT
+            Assert.Equal(expected, actual);
+        }
     }
 }
